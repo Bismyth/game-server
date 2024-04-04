70cc37e5fc610dd3da99adb5f536fc3934075499
Bismyth <woodsy867@gmail.com>
2024-04-04 20:55:27 +0800

    Frontend styling

--- frontend/views/HomeView.vue
+++ frontend/views/HomeView.vue
@@ -15,7 +15,7 @@ const createLobby = () => {
 
 <template>
   <main class="container">
-    <h1 class="title mb-2">Hello</h1>
+    <h1 class="title mb-2">Home</h1>
 
     <div class="mb-2"><button class="button" @click="createLobby">Create Lobby</button></div>
     <div><button class="button" @click="joinLobby">Join Lobby</button></div>
